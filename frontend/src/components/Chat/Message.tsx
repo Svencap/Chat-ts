04c78c623e3d5d9867ce9d381db483a4eee8ebf2
Svencap <Ayzekmanduar@mail.ru>
2023-01-02 10:15:09 +0900

    add message interface

---
+++ frontend/src/components/Chat/Message.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useSelector } from "react-redux";
+
+interface MessageProps {
+  message: { body: string; channelId: number; username: string; id: number };
+}
+
+const Message = ({ message }: MessageProps) => {
+  console.log(message);
+  const { body, id, username } = message;
+  return (
+    <div className="text-break mb-2">
+      <b>{username}</b>: {body}
+    </div>
+  );
+};
+export default Message;
